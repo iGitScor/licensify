6c3ad07fe3c73b13e8c9d79d6b9f00fc9ce13f29
Udey Rishi <udey.rishi@gmail.com>
2016-03-13 15:54:00 -0600

    Adding some more CL args

--- licensify.py
+++ licensify.py
@@ -6,14 +6,15 @@ from licensors.licensor_factory import get_licensor
 
 
 def main(arguments):
-    licensor = get_licensor(arguments.license)
-    licensor.apply_license(arguments.path, arguments.recursive)
+    licensor = get_licensor(arguments.license, arguments.path, arguments.owner, arguments.recursive)
+    licensor.apply_license()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='The path to the project root')
     parser.add_argument('license', help='The name of the license. See config.py')
+    parser.add_argument('owner', help='The owner of the project.')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Enable complete stack trace for unhandled exceptions')
     parser.add_argument('-r', '--recursive', action='store_true', help='Recursively travel inside the project root')
@@ -25,4 +26,4 @@ if __name__ == '__main__':
         if args.debug:
             raise e
         else:
-            print(e, file=sys.stderr)
+            print(e.__class__.__name__ + ': ' + str(e), file=sys.stderr)
